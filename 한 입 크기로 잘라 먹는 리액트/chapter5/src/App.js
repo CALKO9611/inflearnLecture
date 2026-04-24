597242d2a100740a4e25427e07117038aad28aef
CALKO9611 <kimd9669@naver.com>
2026-04-24 16:55:12 +0900

    5. 첫 컴포넌트 만들기

--- 한 입 크기로 잘라 먹는 리액트/chapter5/src/App.js
+++ 한 입 크기로 잘라 먹는 리액트/chapter5/src/App.js
@@ -1,5 +1,24 @@
 import "./App.css";
 
+// 화살표 함수 사용
+const Header = () => {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+};
+
+/* 함수 선언식 사용
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  )
+}
+*/
+
 function App() {
   return <div className="App"></div>;
 }
